5d7c9788d7b2e4d03366bca4551db5fec2e7b084
PALLADIUM26 <pranithdutta26@gmail.com>
2024-08-15 19:55:17 +0530

    Build v0 initialization

---
+++ src/responsiveui/ResponsiveUI.java
@@ -0,0 +1,18 @@
+
+package responsiveui;
+
+/**
+ *
+ * @author User
+ */
+public class ResponsiveUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new MainMenu().setVisible(true);
+        
+    }
+    
+}
